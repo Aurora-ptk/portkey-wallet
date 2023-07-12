636ecb7b01026901558887a2497b14231bf0bf68
ykx <yangkexin@portkey.finance>
2023-07-12 13:52:22 +0800

    feat: 🎸 adapt ui sdk signup and login

--- packages/web-extension-did/app/web/pages/RegisterStart/components/LoginModal/index.tsx
+++ packages/web-extension-did/app/web/pages/RegisterStart/components/LoginModal/index.tsx
@@ -1,35 +1,32 @@
-import CommonModal from 'components/CommonModal';
+import { CommonModal, CreateWalletType, PortkeyStyleProvider } from '@portkey/did-ui-react';
 import { Button } from 'antd';
 
 export default function LoginModal({
   open,
-  type = 'login',
+  type = 'Login',
   onCancel,
   onConfirm,
 }: {
   open?: boolean;
-  type?: string;
+  type?: CreateWalletType;
   onCancel?: () => void;
   onConfirm?: () => void;
 }) {
   return (
-    <CommonModal
-      getContainer={'#register-start-wrapper'}
-      closable={false}
-      open={open}
-      width={320}
-      title={'Continue with this account?'}
-      onCancel={onCancel}>
-      <p className="modal-content">
-        {type === 'login' && 'This account has not been registered yet. Click "Confirm" to complete the registration.'}
-        {type === 'create' && 'This account already exists. Click "Confirm" to log in.'}
-      </p>
-      <div className="btn-wrapper">
-        <Button onClick={onCancel}>Cancel</Button>
-        <Button type="primary" onClick={onConfirm}>
-          Confirm
-        </Button>
-      </div>
-    </CommonModal>
+    <PortkeyStyleProvider>
+      <CommonModal closable={false} open={open} width={320} title={'Continue with this account?'} onCancel={onCancel}>
+        <p className="modal-content">
+          {type === 'Login' &&
+            'This account has not been registered yet. Click "Confirm" to complete the registration.'}
+          {type === 'SignUp' && 'This account already exists. Click "Confirm" to log in.'}
+        </p>
+        <div className="btn-wrapper">
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button type="primary" onClick={onConfirm}>
+            Confirm
+          </Button>
+        </div>
+      </CommonModal>
+    </PortkeyStyleProvider>
   );
 }
