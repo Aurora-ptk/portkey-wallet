828864e83be768c4f5b9c9815714b4fe819ad2b3
ykx <yangkexin@portkey.finance>
2023-07-13 11:10:27 +0800

    test: 💍 modify test yml

--- packages/hooks/hooks-ca/misc.test.ts
+++ packages/hooks/hooks-ca/misc.test.ts
@@ -1,7 +1,15 @@
 import { MiscState } from '../../../test/data/miscState';
 import { renderHookWithProvider } from '../../../test/utils/render';
 import { setupStore } from '../../../test/utils/setup';
-import { useMisc } from './misc';
+import { useMisc, useSetLocalPhoneCountryCode } from './misc';
+import { useAppCommonDispatch } from '../index';
+import { renderHook } from '@testing-library/react';
+import { CountryItem } from '@portkey-wallet/types/types-ca/country';
+import * as MiscActions from '@portkey-wallet/store/store-ca/misc/actions';
+
+jest.mock('../index', () => ({
+  useAppCommonDispatch: jest.fn(),
+}));
 
 describe('useMisc', () => {
   test('get misc data successfully', () => {
@@ -15,5 +23,23 @@ describe('useMisc', () => {
     const { result } = renderHookWithProvider(useMisc, setupStore({}));
 
     expect(result.current).toBeUndefined();
+  });
+});
+
+describe('useSetLocalPhoneCountryCode', () => {
+  it('should call useAppCommonDispatch with setLocalPhoneCountryCodeAction when called', () => {
+    const dispatchMock = jest.fn();
+    const countryItemMock: CountryItem = { country: 'US', code: '+1', iso: 'US' };
+
+    (useAppCommonDispatch as jest.Mock).mockReturnValue(dispatchMock);
+    jest.spyOn(MiscActions, 'setLocalPhoneCountryCodeAction').mockImplementation(jest.fn());
+
+    const { result } = renderHook(() => useSetLocalPhoneCountryCode());
+    const setLocalPhoneCountryCode = result.current;
+
+    setLocalPhoneCountryCode(countryItemMock);
+
+    expect(useAppCommonDispatch).toHaveBeenCalled();
+    expect(MiscActions.setLocalPhoneCountryCodeAction).toHaveBeenCalled();
   });
 });
